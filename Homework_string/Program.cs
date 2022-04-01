2f232075e24857d50e580bc5d7dbc991a53be924
Yana Doll <Dolz_op79@student.itstep.org>
2022-04-01 23:54:28 +0300

    Add project files.

---
+++ Homework_string/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Text;
+namespace New_Language_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //string text = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.Et harum quidem rerum facilis est et expedita distinctio.Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.";
+            //Console.WriteLine(text);
+            //Console.WriteLine();
+
+            //string[] res = text.Split(" ");
+
+            //StringBuilder str = new StringBuilder();
+
+            //for (int i = 0; i < res.Length; i++)
+            //{
+            //    if (res[i].Length < 3)
+            //    {
+            //        res[i] = "secret";
+            //        str.Append(res[i] + " ");
+
+            //    }
+            //    else
+            //    {
+            //        str.Append(res[i] + " ");
+            //    }
+
+            //}
+
+
+            //string str1 = str.ToString();
+
+            //string[] res1 = str1.Split(" ");
+
+            //StringBuilder stroka = new StringBuilder();
+            //for (int i = 0; i < res1.Length; i++)
+            //{
+            //    if (res1[i].Length == 3)
+            //    {
+            //        if (res1[i].IndexOf(",") != -1)
+            //        {
+            //            res1[i] = "secret,";
+            //        }
+            //        if (res1[i].IndexOf(".") != -1)
+            //        {
+            //            res1[i] = "secret.";
+            //        }
+            //        if (res1[i].IndexOf(":") != -1)
+            //        {
+            //            res1[i] = "secret:";
+            //        }
+
+            //        stroka.Append(res1[i] + " ");
+
+            //    }
+            //    else
+            //    {
+            //        stroka.Append(res1[i] + " ");
+            //    }
+
+            //}
+
+            //string text2 = stroka.ToString();
+
+            //Console.WriteLine();
+            //Console.WriteLine(text2);
+            #endregion
+
+            #region Task2
+
+            //string text = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.Et harum quidem rerum facilis est et expedita distinctio.Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.";
+
+            //string text2 = text.Replace(",", " ");
+            //string text3 = text.Replace(".", " ");
+
+            //Console.WriteLine();
+
+
+
+            //string[] res = text3.Split(" ");
+
+            //for (int i = 0; i < res.Length - 1; i++)
+            //{
+            //    for (int j = 0; j < res.Length - 1; j++)
+            //    {
+            //        if (res[j].Length < res[j + 1].Length)
+            //        {
+            //            string time = res[j];
+            //            res[j] = res[j + 1];
+            //            res[j + 1] = time;
+
+            //        }
+            //    }
+
+            //}
+
+            //for (int i = 0; i < res.Length; i++)
+            //{
+            //    Console.WriteLine(res[i]);
+
+            //}
+
+            //Console.WriteLine();
+
+            #endregion
+
+            #region Task3
+
+            //string text = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.Et harum quidem rerum facilis est et expedita distinctio.Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.";
+
+            //Console.WriteLine(text);
+            //Console.WriteLine();
+
+            //string text2 = text.Replace("z", "Z");
+            //string text3 = text2.Replace("s", "S");
+            //string text4 = text3.Replace("c", "C");
+            //Console.WriteLine(text4);
+
+
+            #endregion
+
+            #region Task4
+
+            //string text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur";
+            //Console.WriteLine(text);
+            //Console.WriteLine();
+
+            //string[] res = text.Split(".");
+            //StringBuilder result = new StringBuilder();
+
+            //for (int i = 0; i < res.Length; i++)
+            //{
+            //    result.Append($"\t {char.ConvertFromUtf32(167)} {res[i]}. \n");
+            //}
+            //Console.WriteLine(result);
+
+
+            #endregion
+
+            #region Task5
+
+            string text = "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?";
+            Console.WriteLine(text);
+            Console.WriteLine();
+
+            string text2 = text.Replace("I", "same");
+
+            Console.WriteLine(text2);
+
+
+            #endregion
+        }
+    }
+}
